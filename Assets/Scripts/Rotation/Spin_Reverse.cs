ca656020b9ee0bbd8423b3a8cca91e94bcb490ce
yasiman <yasiman@knights.ucf.edu>
2018-05-20 22:31:34 -0400

    Added new environment assets, added bb scripts to background, made default water sexier

---
+++ Assets/Scripts/Rotation/Spin_Reverse.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin_Reverse : MonoBehaviour
+{
+    public float speed = 10f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.down, speed * Time.deltaTime);
+    }
+}
